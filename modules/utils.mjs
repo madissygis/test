2c043b188b34dca97f6f622b8acbfff88853a01e
madissygis <madissygis@gmail.com>
2025-06-27 16:33:47 +0300

    Update utils.mjs

--- modules/utils.mjs
+++ modules/utils.mjs
@@ -1,6 +1,9 @@
 export function debug_log(msg) {
   const logElem = document.getElementById('log');
-  if (logElem) {
-    logElem.textContent += msg + "\n";
-  }
+  if (logElem) logElem.textContent += msg + "\n";
+}
+
+export function clear_log() {
+  const logElem = document.getElementById('log');
+  if (logElem) logElem.textContent = "";
 }
