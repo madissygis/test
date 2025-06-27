a5845c979773dc9a41ed252f879d5c4b61f8fdd2
madissygis <madissygis@gmail.com>
2025-06-27 18:46:23 +0300

    Delete modules directory

--- modules/utils.mjs
+++
@@ -1,9 +0,0 @@
-export function debug_log(msg) {
-  const logElem = document.getElementById('log');
-  if (logElem) logElem.textContent += msg + "\n";
-}
-
-export function clear_log() {
-  const logElem = document.getElementById('log');
-  if (logElem) logElem.textContent = "";
-}
