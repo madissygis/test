269a43df5aa15c99791df4c4eb596fe9c6bf167c
madissygis <madissygis@gmail.com>
2025-06-30 00:53:22 +0300

    Update int64.js

--- int64.js
+++ int64.js
@@ -1,23 +1,19 @@
-function Int64(lo, hi) {
-  this.lo = lo >>> 0;
-  this.hi = hi >>> 0;
+let ropChain = null;
+
+function setupROP() {
+  ropChain = new Uint32Array(0x400);
+  let i = 0;
+
+  // Fill with dummy values for now
+  for (; i < ropChain.length; i++) {
+    ropChain[i] = 0x90909090;
+  }
+
+  debug_log("ROP chain initialized.");
+  return ropChain;
 }
 
-Int64.prototype.toString = function () {
-  return '0x' + this.hi.toString(16).padStart(8, '0') + this.lo.toString(16).padStart(8, '0');
-};
-
-Int64.prototype.asDouble = function () {
-  var buffer = new ArrayBuffer(8);
-  var u32 = new Uint32Array(buffer);
-  u32[0] = this.lo;
-  u32[1] = this.hi;
-  return new Float64Array(buffer)[0];
-};
-
-Int64.fromDouble = function (d) {
-  var buffer = new ArrayBuffer(8);
-  new Float64Array(buffer)[0] = d;
-  var u32 = new Uint32Array(buffer);
-  return new Int64(u32[0], u32[1]);
-};
+function getROPChainAddr() {
+  // Placeholder (will need real heap leak or aligned placement)
+  return 0x133713370000;
+}
