609746128043678887391a53df67516c72ceb52b
madissygis <madissygis@gmail.com>
2025-06-30 00:53:09 +0300

    Update int64.js

--- int64.js
+++ int64.js
@@ -1,64 +1,23 @@
-function Int64(v) {
-    var bytes = new Uint8Array(8);
-    switch (typeof v) {
-        case 'number':
-            v = '0x' + Math.floor(v).toString(16);
-        case 'string':
-            if (v.startsWith('0x')) v = v.substr(2);
-            if (v.length % 2 == 1) v = '0' + v;
-            var bigEndian = unhexlify(v, 8);
-            bytes.set(Array.from(bigEndian).reverse());
-            break;
-        case 'object':
-            if (v instanceof Int64) {
-                bytes.set(v.bytes());
-            } else {
-                if (v.length != 8)
-                    throw TypeError("Array must have exactly 8 elements.");
-                bytes.set(v);
-            }
-            break;
-        case 'undefined':
-            break;
-        default:
-            throw TypeError("Int64 constructor requires an argument.");
-    }
-    this.asDouble = function() {
-        if (bytes[7] == 0xff && (bytes[6] == 0xff || bytes[6] == 0xfe))
-            throw new RangeError("Integer can not be represented by a double");
-        return Struct.unpack(Struct.float64, bytes);
-    };
-    this.toString = function() {
-        return '0x' + hexlify(Array.from(bytes).reverse());
-    };
-    this.bytes = function() {
-        return Array.from(bytes);
-    };
-    this.byteAt = function(i) {
-        return bytes[i];
-    };
-    this.assignAdd = function(a, b) {
-        var carry = 0;
-        for (var i = 0; i < 8; i++) {
-            var cur = a.byteAt(i) + b.byteAt(i) + carry;
-            carry = cur > 0xff | 0;
-            bytes[i] = cur;
-        }
-        return this;
-    };
-    this.assignSub = function(a, b) {
-        var carry = 0;
-        for (var i = 0; i < 8; i++) {
-            var cur = a.byteAt(i) - b.byteAt(i) - carry;
-            carry = cur < 0 | 0;
-            bytes[i] = cur;
-        }
-        return this;
-    };
+function Int64(lo, hi) {
+  this.lo = lo >>> 0;
+  this.hi = hi >>> 0;
 }
-Int64.fromDouble = function(d) {
-    var bytes = Struct.pack(Struct.float64, d);
-    return new Int64(bytes);
+
+Int64.prototype.toString = function () {
+  return '0x' + this.hi.toString(16).padStart(8, '0') + this.lo.toString(16).padStart(8, '0');
 };
-Int64.Zero = new Int64(0);
-Int64.One = new Int64(1);
+
+Int64.prototype.asDouble = function () {
+  var buffer = new ArrayBuffer(8);
+  var u32 = new Uint32Array(buffer);
+  u32[0] = this.lo;
+  u32[1] = this.hi;
+  return new Float64Array(buffer)[0];
+};
+
+Int64.fromDouble = function (d) {
+  var buffer = new ArrayBuffer(8);
+  new Float64Array(buffer)[0] = d;
+  var u32 = new Uint32Array(buffer);
+  return new Int64(u32[0], u32[1]);
+};
